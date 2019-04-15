5c32df4b57698297fe37cb4eb0ec652bcbed10aa
Crayssac <paul-ka@laposte.net>
2019-04-15 11:53:14 +0200

    Add feedback when gamer make a mistake

--- data.js
+++ data.js
@@ -1,7 +1,7 @@
 var mydata=[
     {name:"base"},
     {name:"action 1", cond:"0"},
-    {name:"action 2", cond:"1"},
-    {name:"action 3", cond:"1"},
-    {name:"action 4", cond:"2,3"}
+    {name:"action 2", cond:"1", fb:"Feedback si la condition 1 n'est pas sélectionnée"},
+    {name:"action 3", cond:"1", fb:"Feedback si la condition 1 n'est pas sélectionnée"},
+    {name:"action 4", cond:"2,3", fb:"Feedback si les conditions 2 et 3 ne sont pas sélectionnée"}
 ];
